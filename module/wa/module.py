906c598ae29909a111bf804b0177627c69333809
Grigori Fursin <Grigori.Fursin@cTuning.org>
2016-06-06 12:00:53 +0200

    added ck import wa function to import all workloads to CK format; imported all workloads

--- module/wa/module.py
+++ module/wa/module.py
@@ -12,6 +12,7 @@ work={} # Will be updated by CK (temporal data)
 ck=None # Will be updated by CK (initialized CK kernel) 
 
 # Local settings
+line='*************************************************************************************'
 
 ##############################################################################
 # Initialize module
@@ -156,7 +157,7 @@ def dashboard(i):
 ##############################################################################
 # import workloads from original WA
 
-def import(i):
+def wa_import(i):
     """
     Input:  {
             }
@@ -169,14 +170,211 @@ def import(i):
 
     """
 
+    import os
+    import copy
+
+    o=i.get('out','')
+    oo=''
+    if o=='con': oo=o
+
     # Get platform params
     hos=i.get('host_os','')
     tos=i.get('target_os', '')
     tdid=i.get('device_id', '')
 
+    ck.out('Detecting host and target platform info ...')
+    ck.out('')
+
+    ii={'action':'detect',
+        'module_uoa':cfg['module_deps']['platform'],
+        'out':oo,
+        'host_os':hos,
+        'target_os':tos,
+        'target_device_id':tdid}
+    rpp=ck.access(ii)
+    if rpp['return']>0: return rpp
+
+    hos=rpp['host_os_uoa']
+    hosd=rpp['host_os_dict']
+
+    tos=rpp['os_uoa']
+    tosd=rpp['os_dict']
+    tbits=tosd.get('bits','')
+
+    hosz=hosd.get('base_uoa','')
+    if hosz=='': hosz=hos
+    tosz=tosd.get('base_uoa','')
+    if tosz=='': tosz=tos
+
+    remote=tosd.get('remote','')
+
+    tdid=rpp['device_id']
+
+    if hos=='':
+       return {'return':1, 'error':'"host_os" is not defined or detected'}
+
+    if tos=='':
+       return {'return':1, 'error':'"target_os" is not defined or detected'}
+
+    # Various host and target vars
+    bbp=hosd.get('batch_bash_prefix','')
+    rem=hosd.get('rem','')
+    eset=hosd.get('env_set','')
+    etset=tosd.get('env_set','')
+    svarb=hosd.get('env_var_start','')
+    svarb1=hosd.get('env_var_extra1','')
+    svare=hosd.get('env_var_stop','')
+    svare1=hosd.get('env_var_extra2','')
+    scall=hosd.get('env_call','')
+    sdirs=hosd.get('dir_sep','')
+    sdirsx=tosd.get('remote_dir_sep','')
+    if sdirsx=='': sdirsx=sdirs
+    stdirs=tosd.get('dir_sep','')
+    sext=hosd.get('script_ext','')
+    sexe=hosd.get('set_executable','')
+    se=tosd.get('file_extensions',{}).get('exe','')
+    sbp=hosd.get('bin_prefix','')
+    stbp=tosd.get('bin_prefix','')
+    sqie=hosd.get('quit_if_error','')
+    evs=hosd.get('env_var_separator','')
+    envsep=hosd.get('env_separator','')
+    envtsep=tosd.get('env_separator','')
+    eifs=hosd.get('env_quotes_if_space','')
+    eifsc=hosd.get('env_quotes_if_space_in_call','')
+    eifsx=tosd.get('remote_env_quotes_if_space','')
+    if eifsx=='': eifsx=eifsc
+    wb=tosd.get('windows_base','')
+    stro=tosd.get('redirect_stdout','')
+    stre=tosd.get('redirect_stderr','')
+    ubtr=hosd.get('use_bash_to_run','')
+    no=tosd.get('no_output','')
+    bex=hosd.get('batch_exit','')
+    md5sum=hosd.get('md5sum','')
+
     # Set environment for WA
+    ck.out('')
+    ck.out('Setting CK environment for ARM workload automation ...')
+
+    ii={'action':'set',
+        'module_uoa':cfg['module_deps']['env'],
+        'host_os':hos,
+        'target_os':tos,
+        'target_device_id':tdid,
+        'tags':'wa'}
+    r=ck.access(ii)
+    if r['return']>0: return r
+
+    bat=r['bat']
+
+    # Prepare tmp bat file and tmp output file
+    rx=ck.gen_tmp_file({'prefix':'tmp-', 'suffix':sext})
+    if rx['return']>0: return rx
+    fbat=rx['file_name']
+
+    rx=ck.gen_tmp_file({'prefix':'tmp-', 'suffix':'.tmp'})
+    if rx['return']>0: return rx
+    ftmp=rx['file_name']
+
+    # Write bat file
+    bat+='\n'
+    bat+='wa list workloads > '+ftmp
+
+    rx=ck.save_text_file({'text_file':fbat, 'string':bat})
+    if rx['return']>0: return rx
+
+    y=''
+    if sexe!='':
+       y+=sexe+' '+fbat+envsep
+    y+=' '+scall+' '+fbat
+
+    if ubtr!='': y=ubtr.replace('$#cmd#$',y)
+
+    ck.out('')
+    ck.out('Executing '+bat+' ...')
+
+    os.system(y)
+
+    # Read and delete tmp file with WA description
+    rz=ck.load_text_file({'text_file':ftmp, 'split_to_list':'yes', 'delete_after_read':'yes'})
+    if rz['return']>0: return rz
+    lst=rz['lst']
+
+    # Delete fbat (temp)
+    if os.path.isfile(fbat):
+       os.remove(fbat)
+
+    # Parse descriptions
+    wa={}
+    wk=''
+    wv=''
+    for l in lst:
+        if l!='':
+           j=l.find(': ')
+           if j>0:
+              if wk!='':
+                 wa[wk]=wv
+                 wk=''
+                 wv=''
+              wk=l[:j].strip()
+              wv=l[j+2:].strip()
+           else:
+              wv+=' '+l.strip()
+    if wk!='':
+       wa[wk]=wv
+
+    # Add entries
+    for w in wa:
+        wd=wa[w]
+
+        # First check, if such program exists
+        ii={'action':'load',
+            'module_uoa':cfg['module_deps']['program'],
+            'data_uoa':w}
+        r=ck.access(ii)
+        if r['return']>0:
+           if r['return']!=16: return r # some other error (16 - entry not found)
+
+           # Preparing meta in CK program format
 
 
+           # Adding new entry
+           ck.out('  Adding new entry '+w+' ...')
 
+           rx=ck.gen_uid({})
+           if rx['return']>0: return rx
+           duid=rx['data_uid']
+
+           d=copy.deepcopy(cfg['wa_template'])
+
+           d["backup_data_uid"]=duid
+           d["data_name"]=w
+           d["wa_desc"]=wd
+
+           x=d["print_files_after_run"]
+           xx=[]
+           for y in x:
+               xx.append(y.replace('$#wa_name#$',w))
+           d["print_files_after_run"]=xx
+
+           x=d["run_cmds"]["default"]["run_time"]["run_cmd_main"]
+           d["run_cmds"]["default"]["run_time"]["run_cmd_main"]=x.replace('$#wa_name#$',w)
+
+           x=d["tags"]
+           xx=[]
+           for y in x:
+               xx.append(y.replace('$#wa_name#$',w))
+           d["tags"]=xx
+
+           ii={'action':'add',
+               'module_uoa':cfg['module_deps']['program'],
+               'data_uoa':w,
+               'data_uid':duid,
+               'repo_uoa':'ck-wa',
+               'dict':d}
+           r=ck.access(ii)
+           if r['return']>0: return r
+
+        else:
+           ck.out('    Entry '+w+' already exists!')
 
     return {'return':0}
---
+++ program/.cm/alias-u-182a73e022d2f22c
@@ -0,0 +1,1 @@
+angrybirds
